3cf01a34d2f5872257f17e1fca91379063fa625c
roxanmlr <roxanmlr@gmail.com>
2026-01-07 23:48:26 +0100

    PermissionVerificationServiceImpl Test

--- src/test/java/fr/techcrud/pmt_api/services/PermissionVerificationServiceImplTest.java
+++ src/test/java/fr/techcrud/pmt_api/services/PermissionVerificationServiceImplTest.java
@@ -19,7 +19,7 @@ import static org.assertj.core.api.Assertions.assertThat;
 import static org.mockito.Mockito.*;
 
 @ExtendWith(MockitoExtension.class)
-class permissionVerificationServiceImplTest {
+class PermissionVerificationServiceImplTest {
 
     @Mock
     private RolePermissionRepository rolePermissionRepository;
